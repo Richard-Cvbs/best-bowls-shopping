a93c75c96ccaa19d8bf4cf7b72460ba1911392bb
Diego Felix <dt196397@gmail.com>
2022-03-15 13:00:17 -0600

    set up app + bootsrap + html skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar m-1 navbar-light bg-light">
+      <div className="container-fluid">
+        <span className='text-secondary fw-bold'>
+          <i className="bi bi-egg-fried"></i>
+          <a className="navbar-brand ms-1" href="#">Best In Bowls!!</a>
+        </span>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id='main-nav'></div>
+      </div>
+    </nav>
     </div>
   );
 }

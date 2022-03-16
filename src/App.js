669fd32fe78628a2a3d8eba932300410cf23c16c
Diego Felix <dt196397@gmail.com>
2022-03-15 19:01:45 -0600

    set up Offcanvas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, {useState} from 'react'
 import './App.css';
 import { Navbar,Nav,Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import MyComponent from './components/MyComponent';
+import PurchaseCart from './components/PurchaseCart';
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
       <Navbar bg="light" expand="md">
@@ -24,20 +31,23 @@ function App() {
           <Nav.Link className='d-md-none' href="#link">
             <i className="bi bi-cart me-1"></i>
             Cart</Nav.Link>
-          <Button className='d-none d-md-inline' variant="success">
+          <Button onClick={handleShow} className='d-none d-md-inline' variant="success">
           <i className="bi bi-cart me-1"></i>
             Cart</Button>
         </Nav>
       </Navbar.Collapse>
         </Container>
-        
       </Navbar>
+
+      <PurchaseCart  show={show} handleClose={handleClose}/>
+
       <Container fluid>
       <h2 className="bg-success p-1 text-warning text-center text-decoration-underline">Pick your preffered Bowls!!</h2>
         </Container>
       <Container>
       <MyComponent/>
       </Container>
+
     </div>
   );
 }
